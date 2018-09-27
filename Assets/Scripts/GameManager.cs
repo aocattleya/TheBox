c578d7600d23a6433cd69ae38585a99bab088484
aocattleya <aocattleya@yahoo.co.jp>
2018-09-28 05:25:21 +0900

    アイテムの仕掛け配置(途中)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,27 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     // 定数定義：壁方向
     public const int WALL_FRONT = 1;    // 前
     public const int WALL_RIGHT = 2;    // 右
-    public const int WALL_BACK = 3; // 後
-    public const int WALL_LEFT = 4; // 左
+    public const int WALL_BACK = 3;     // 後
+    public const int WALL_LEFT = 4;     // 左
 
-    public GameObject panelWalls;
+    public GameObject panelWalls;       // 壁全体
+
+    public GameObject buttonMassage;        // ボタン：メッセージ
+    public GameObject buttonMassageText;    // メッセージテキスト
 
     private int wallNo; // 現在の向いている方向
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         wallNo = WALL_FRONT;    // スタート時点では「前」を向く
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // メモをタップ
+    public void PushButtonMemo()
+    {
+        DisplayMessage("エッフェル塔と書いてある。");
+    }
+
+    // メッセージをタップ
+    public void PushButtonMessage()
+    {
+        buttonMassage.SetActive(false);     // メッセージを消す
+    }
 
     // 右(>)ボタンを押した
     public void PushButtonRight()
@@ -42,11 +60,18 @@ public class GameManager : MonoBehaviour {
     {
         wallNo--;   // 方向を一つ左に
         // 「前」の一つ左は「左」
-        if(wallNo < WALL_FRONT)
+        if (wallNo < WALL_FRONT)
         {
             wallNo = WALL_LEFT;
         }
         DisplayWall();  // 壁表示更新
+    }
+
+    // メッセージを表示
+    void DisplayMessage(string mes)
+    {
+        buttonMassage.SetActive(true);
+        buttonMassageText.GetComponent<Text>().text = mes;
     }
 
     // 向いている方向の壁を表示
