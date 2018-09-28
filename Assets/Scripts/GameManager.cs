2957661b5f9f90ade148f46bed1596e81be20020
aocattleya <aocattleya@yahoo.co.jp>
2018-09-29 05:04:49 +0900

    金庫のボタン仕掛け完成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,20 +9,43 @@ public class GameManager : MonoBehaviour
     // 定数定義：壁方向
     public const int WALL_FRONT = 1;    // 前
     public const int WALL_RIGHT = 2;    // 右
-    public const int WALL_BACK = 3;     // 後
-    public const int WALL_LEFT = 4;     // 左
+    public const int WALL_BACK  = 3;    // 後
+    public const int WALL_LEFT  = 4;    // 左
+
+    // 定数定義：ボタンカラー
+    public const int COLOR_GREEN = 0;   // 緑
+    public const int COLOR_RED   = 1;   // 赤
+    public const int COLOR_BLUE  = 2;   // 青
+    public const int COLOR_WHITE = 3;   // 白
 
     public GameObject panelWalls;       // 壁全体
+
+    public GameObject buttonHammer;     // ボタン：トンカチ
+
+    public GameObject imageHammerIcon;  // アイコン：トンカチ
 
     public GameObject buttonMassage;        // ボタン：メッセージ
     public GameObject buttonMassageText;    // メッセージテキスト
 
-    private int wallNo; // 現在の向いている方向
+    public GameObject[] buttonLamp = new GameObject[3]; // ボタン：金庫
+
+    public Sprite[] buttonPicture = new Sprite[4];    // ボタンの絵
+
+    public Sprite hammerPicture;            // トンカチの絵
+
+    private int wallNo;                     // 現在の向いている方向
+    private bool doesHaveHammer;            // トンカチを持っているか
+    private int[] buttonColor = new int[3]; // 金庫のボタン
 
     // Use this for initialization
     void Start()
     {
-        wallNo = WALL_FRONT;    // スタート時点では「前」を向く
+        wallNo = WALL_FRONT;            // スタート時点では「前」を向く
+        doesHaveHammer = false;         // トンカチは「持っていない」
+
+        buttonColor[0] = COLOR_GREEN;   // ボタン1の色は「緑」
+        buttonColor[1] = COLOR_RED;     // ボタン2の色は「赤」
+        buttonColor[2] = COLOR_BLUE;    // ボタン3の色は「青」
     }
 
     // Update is called once per frame
@@ -31,10 +54,64 @@ public class GameManager : MonoBehaviour
 
     }
 
+    // 金庫のボタン1をタップ
+    public void PushButtonLamp1()
+    {
+        ChangeButtonColor(0);
+    }
+
+    // 金庫のボタン2をタップ
+    public void PushButtonLamp2()
+    {
+        ChangeButtonColor(1);
+    }
+
+    // 金庫のボタン3をタップ
+    public void PushButtonLamp3()
+    {
+        ChangeButtonColor(2);
+    }
+
+    // 金庫のボタンの色を変更
+    void ChangeButtonColor(int buttonNo)
+    {
+        buttonColor[buttonNo]++;
+        // 「白」の時にボタンを押したら「緑」に
+        if (buttonColor[buttonNo] > COLOR_WHITE)
+        {
+            buttonColor[buttonNo] = COLOR_GREEN;
+        }
+        // ボタンの画像を変更
+        buttonLamp[buttonNo].GetComponent<Image>().sprite =
+            buttonPicture[buttonColor[buttonNo]];
+
+        // ボタンの色順をチェック
+        if ((buttonColor[0] == COLOR_BLUE) &&
+            (buttonColor[1] == COLOR_WHITE) &&
+            (buttonColor[2] == COLOR_RED))
+        {
+            // まだトンカチを持っていない？
+            if (doesHaveHammer == false)
+            {
+                DisplayMessage("金庫にトンカチが入っていた。");
+                buttonHammer.SetActive(true);   // トンカチの絵を表示
+                imageHammerIcon.GetComponent<Image>().sprite = hammerPicture;
+
+                doesHaveHammer = true;
+            }
+        }
+    }
+
     // メモをタップ
     public void PushButtonMemo()
     {
         DisplayMessage("エッフェル塔と書いてある。");
+    }
+
+    // トンカチの絵をタップ
+    public void PushButtonHammer()
+    {
+        buttonHammer.SetActive(false);
     }
 
     // メッセージをタップ
